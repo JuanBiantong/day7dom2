8ac9bd0e3457f3da416d66c7bdd966f63a46bb49
JuanBiantong <jbiantong@gmail.com>
2020-06-17 02:11:42 +0700

    first commit

---
+++ dom2.js
@@ -0,0 +1,4 @@
+document.getElementById('p1').style.color = '#f2f2f2f2';
+document.getElementById('p1').style.background = 'salmon';
+document.getElementById('p1').style.fontSize = '50px';
+document.getElementById('p1').style.fontFamily = 'Arial, Helvetica, sans-serif';
